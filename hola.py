6efa0e1cbfb142a249d0953806af63ce02acb3ca
nicolagg23 <nicolag.rubilar@gmail.com>
2025-04-24 15:35:20 -0400

    clase 24-04 primer comit

---
+++ hola.py
@@ -0,0 +1,63 @@
+# print("Ingrese su nombre")
+# nombre=input()
+# print("Ingrese su edad")
+# # edad=input()
+# edad=int(input())
+
+# #print("Hola", nombre, "su edad es",edad)
+# print(f"Hola {nombre} su edad es {edad}")
+
+## mayor o menor de edad
+# if edad>=18:
+#     print("Usted es mayor de edad")
+# else:
+#     print("Usted no es mayor de edad")
+
+
+
+# n1=int(input("ingrese un numero"))
+# n2=int(input("ingrese un numero"))
+# print("El resultado es ", n1+n2)
+
+##diferentes edades
+# print("Ingrese edad")
+# edad=int(input())
+# if edad<12:
+#     print("Usted es niño")
+# elif edad>=12 and edad <=17:
+#     print("Usted es adolecente")
+# elif edad>=18 and edad <=64:
+#     print("Usted es adulto")
+# else:
+#     print ("usted es adulto mayor")
+
+## clave secreta
+# clave=3344
+# print("Ingrese su clave")
+# pas=int(input())
+# if pas==clave:
+#     print("Bienvenido al sistema")
+# else:
+#     printe("Clave invalida")
+
+## numeros par e inpar
+# num=int(input("Ingrese un numero"))
+# if num% 2==0:
+#     print("El numero es par")
+# else:
+#     print("El numero es impar")
+
+## colores
+# color=input("Ingrese un color")
+
+# if color =="azul":
+#     print("Es de la U")
+# elif color==blanco:
+#     print("Es de Colo Colo")
+# elif color==rojo:
+#     print("Es de union española")
+# elif color==verde:
+#     print("Es de audax")
+# else:
+#     print("Color no valido")
+
